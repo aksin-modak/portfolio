a06f0fd38650dab873112de1f3a1751cd4f21778
Devashish Pawar <aksin.modak@gmail.com>
2023-12-14 19:55:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function my_home(){
+  alert("Welcome to Home Page!!!");
+}
+function my_about(){
+  alert("Welcome to About Page!!!");
+}
+function my_skills(){
+  alert("Welcome to Skills Page!!!");
+}
+function my_projects(){
+  alert("Welcome to Projects Page!!!");
+}
+function my_contact(){
+  alert("Welcome to Contact Me Page!!!");
+}
+function my_resume(){
+  alert("Resume will be downloaded shortly");
+}
